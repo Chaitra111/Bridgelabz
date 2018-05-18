4e641cd6195fa3829c7ce6ba19070e206cc229cf
Chaitra111 <111chaitra@gmail.com>
2018-05-18 10:58:09 +0530

    first commit

---
+++ ReplaceUsername.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.FunctioalPrograming;
+
+import com.bridgelabz.Utility.Utility;
+
+/**
+ * @author Chaitra Ankolekar
+ * 
+ * Date:18/05/2018
+ * 
+ * Purpose:Get the Input and Replace String Template “Hello <<UserName>>, How are you?”  
+ */
+public class ReplaceUsername 
+{
+	public static void main(String[] args) 
+	{
+		Utility utility = new Utility();
+		String name=utility.getString();
+		String msg = "Hello <<username>>, How are you?";
+		
+		if(name.length()>=3) 
+		{
+		    System.out.println(msg.replace("<<username>>",name));
+		}
+		else
+		{
+			System.out.println("Invalid input");
+		} 
+		} }
