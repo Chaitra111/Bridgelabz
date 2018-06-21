b620cd6fbbd557e79212d16aadb3f351dd48ef28
Chaitra111 <111chaitra@gmail.com>
2018-06-21 10:49:02 +0530

    commit

---
+++ Node.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.DataStructure;
+
+public class Node<T> {
+	public T data;
+	public Node prev;
+	public Node next;
+	protected Node link;
+    public Node top;
+	public Node(T data) {
+		this.data = (T) data;
+	}
+
+	public Node(T data, Node prev, Node next) {
+		this.data = data;
+		this.prev = prev;
+		this.next = next;
+	}
+
+	public Node(T data, Node link) {
+		this.data = data;
+		this.link = link;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public T getData() {
+		return data;
+	}
+
+	/**
+	 * @param data
+	 *            the data to set
+	 */
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public void setLink(Node n) {
+		link = n;
+	}
+
+	public Node getLink() {
+		return link;
+	}
+}
